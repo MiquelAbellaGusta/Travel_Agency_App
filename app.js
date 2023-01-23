82545a955dfdbafb7377c93c125635876f0bb9bf
MiquelAbellaG <miquel.abella.dw@immune.institute>
2023-01-23 13:07:18 +0100

    base app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+//RUTAS
+
+app.use('/api', require('.routes/api'));
+
+
+module.exports = app;
---
+++ config/db.js
@@ -0,0 +1,5 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(process.env.SQL_URI);
+
+global.db = pool.promise();
